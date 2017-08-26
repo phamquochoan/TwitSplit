55bd9ed7776ddef5eb314a6613a6b6fea513634b
HoanPQ <hoanpq@icloud.com>
2017-08-26 17:56:36 +0700

    Add config file

---
+++ TwitSplit/Config/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  TwitSplit
+//
+//  Created by Hoan Pham on 8/26/17.
+//  Copyright © 2017 Hoan Pham. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    static let TweetLength: Int = 50
+    static let Joinner: String = "/"
+}
+
