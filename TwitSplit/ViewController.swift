ab2f4fc8bdae140ea1b67c358cc403cec7b2a94d
HoanPQ <hoanpq@icloud.com>
2017-08-26 17:37:48 +0700

    Initial commit

---
+++ TwitSplit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitSplit
+//
+//  Created by Hoan Pham on 8/26/17.
+//  Copyright © 2017 Hoan Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
